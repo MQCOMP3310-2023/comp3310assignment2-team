caf02a294b58b04724a64a78f5f12272d47518ae
awarnecke <awarnecke002@gmail.com>
2023-05-11 14:12:57 +1000

    Document vulnerability fix

--- project/json.py
+++ project/json.py
@@ -9,6 +9,7 @@ json = Blueprint('json', __name__)
 #JSON APIs to view Restaurant Information
 @json.route('/restaurant/<restaurant_id>/menu/JSON')
 def restaurantMenuJSON(restaurant_id):
+    # An SQL injection vulnerability was found and fixed here, by switching from string concatenation to SQL parameterisation
     items = db.session.execute(text('select * from menu_item where restaurant_id = %s'), (restaurant_id))
     items_list = [ i._asdict() for i in items ]
     return pyjs.dumps(items_list)
@@ -16,6 +17,7 @@ def restaurantMenuJSON(restaurant_id):
 
 @json.route('/restaurant/<restaurant_id>/menu/<int:menu_id>/JSON')
 def menuItemJSON(restaurant_id, menu_id):
+    # An SQL injection vulnerability was found and fixed here, by switching from string concatenation to SQL parameterisation
     Menu_Item = db.session.execute(text('select * from menu_item where id = %s limit 1'), (str(menu_id)))
     items_list = [ i._asdict() for i in Menu_Item ]
     return pyjs.dumps(items_list)
